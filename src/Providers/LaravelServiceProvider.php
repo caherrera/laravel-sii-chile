ad9d9e712f058f648f7275e23d478ee5b7e44ab3
Carlos Herrera <carlos.herrera@itlinesoftware.com>
2018-09-28 13:47:18 -0300

    initial commit

---
+++ src/Providers/LaravelServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlosherrera
+ * Date: 28/9/18
+ * Time: 1:29 PM
+ */
+
+namespace Caherrera\Lavsii;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register bindings in the container.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+	}
+
+
+}
